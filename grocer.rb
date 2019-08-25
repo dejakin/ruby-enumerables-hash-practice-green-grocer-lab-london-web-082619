a2441dfdcbb34dc99d60cd9ff7be4c184ea991c6
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-08-25 01:30:43 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,7 +19,9 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    item = coupon[:item]
+    if cart[item] && coupon[:item][:count] >= coupon[:num]
 end
 
 def apply_clearance(cart)
