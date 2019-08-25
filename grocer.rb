8c320e60295ea17da4a08905f07705b8da902cba
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-08-25 03:21:13 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,7 +41,7 @@ end
 cart
 end
 
-def checkout(cart, coupons)
+def checkout(array, coupons)
   hash_cart = consolidate_cart(array)
   applied_coupons = apply_coupons(hash_cart, coupons)
   applied_discount = apply_clearance(applied_coupons)
