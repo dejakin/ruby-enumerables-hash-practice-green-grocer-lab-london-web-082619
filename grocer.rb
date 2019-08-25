60b9ed5a2d671ec7ea49265f45de5334a70a69bf
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-08-25 02:32:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,5 +41,9 @@ cart
 end
 
 def checkout(cart, coupons)
-  # code here
+  hash_cart = consolidate_cart(array)
+  applied_coupons = apply_coupons(hash_cart, coupons)
+  applied_discount = apply_clearance(applied_coupons)
+  total = applied_discount.reduce(0) { |acc, (key, value)| acc += value[:prize] * value[:count]}
+  total > 100 ? total * 0.9
 end
