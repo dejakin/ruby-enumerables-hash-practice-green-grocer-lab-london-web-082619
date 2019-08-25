718bf4abdf5d7b702d7e3bca8d2d830f0796f48b
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-08-25 02:52:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -23,7 +23,7 @@ def apply_coupons(cart, coupons)
     item = coupon[:item]
     if cart[item] 
       if !cart["#{item} W/COUPON"] && cart[item][:count] >= coupon[:num]
-         cart["#{item} W/COUPON"] = {count: coupon[:num], / coupon[:num], clearence: cart[item][:clearance], count: coupon[:num]}
+         cart["#{item} W/COUPON"] = {count: coupon[:num], price: coupon[:cost] / coupon[:num], clearence: cart[item][:clearance], count: coupon[:num]}
     cart[item][:count] -= coupon[:num]
     elsif cart[item][:count] >= coupon[:num] && cart["#{item} W/COUPON"]
     cart["#{item} W/COUPON"][:count] += coupon[:num]
